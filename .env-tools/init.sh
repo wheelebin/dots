4e21ebe1c0a54e0f4bb008d5657a6aa83bec7677
wheelebin <jln.allen95@gmail.com>
2025-08-30 00:02:38 +0200

    feat: added init step to set remote url

--- .env-tools/init.sh
+++ .env-tools/init.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-
 shopt -s expand_aliases
 set -euo pipefail
-
-readonly DOTS_REPO="git@github.com:wheelebin/dots.git"
-readonly DOTS_DIR="${HOME}/.cfg"
 
 function log() {
     echo "[$1]: $2" >&2;
 }
 
-shellConfigHasAlias=false
+# Choose repo remote url
+dots_dir="${HOME}/.cfg"
+dots_repo_type=${1:-ssh}
+
+if [[ "${dots_repo_type}" == "https" ]]; then
+    dots_repo="https://github.com/wheelebin/dots.git"
+elif [[ "${dots_repo_type}" == "ssh" ]]; then
+    dots_repo="git@github.com:wheelebin/dots.git"
+else
+    log ERROR "Invalid repo type: ${dots_repo_type} (Valid types are ssh, https)"
+    exit 1
+fi
 
 # Find shell config
 for config in "${HOME}/.zshrc" "${HOME}/.bashrc"; do
@@ -25,14 +32,14 @@ if [[ -z "${shell_config:-}" ]]; then
     exit 1;
 fi
 
-log INFO "Using $shell_config"
+log INFO "Using ${shell_config}"
 
 # Add alias if not exists
-if grep -q "alias config=" "$shell_config" 2>/dev/null; then
+if grep -q "alias config=" "${shell_config}" 2>/dev/null; then
     log INFO "Alias already configured (skipping)"
 else
     log INFO "Adding alias to config"
-    echo "alias config='/usr/bin/git --git-dir=\$HOME/.cfg/ --work-tree=\$HOME'" >> "$shell_config"
+    echo "alias config='/usr/bin/git --git-dir=\$HOME/.cfg/ --work-tree=\$HOME'" >> "${shell_config}"
 fi
 
 # Add to gitignore if not exists
@@ -44,11 +51,11 @@ else
 fi
 
 # Clone repo if not exists
-if [[ -d "$DOTS_DIR" ]]; then
+if [[ -d "${dots_dir}" ]]; then
     log INFO "Dots repo already exists (skipping)"
 else
     log INFO "Cloning dots repo"
-    git clone --bare "$DOTS_REPO" "$DOTS_DIR"
+    git clone --bare "${dots_repo}" "${dots_dir}"
 
     alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
